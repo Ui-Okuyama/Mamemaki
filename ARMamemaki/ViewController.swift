20a824999c9833c3d2fe07a1f15d17e71bdb6f9b
Ui-Okuyama <uiokuyama1997@gmail.com>
2022-01-26 16:37:03 +0900

    edit experience

--- ARMamemaki/ViewController.swift
+++ ARMamemaki/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ARMamemaki
 //
-//  Created by ゲストさん on 2022/01/26.
+//  Created by clairnookuyama on 2022/01/26.
 //
 
 import UIKit
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let mameAnchor = try! Experience.loadMame()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(mameAnchor)
     }
 }
